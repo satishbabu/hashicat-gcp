26decb0ab8fcad1f266c07748652abd626b98cd9
satishbabu <reachsatish@yahoo.com>
2021-05-19 17:57:49 +0000

    test

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@
 #
 # Here is where we store the default values for all the variables used in our
 # Terraform code. If you create a variable with no default, the user will be
-# prompted to enter it (or define it via config file or command line flags.)
+# prompted to enter it (or define it via config file or command line flags.)test
 
 variable "project" {
   description = "ID of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
